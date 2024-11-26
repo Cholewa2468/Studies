41ca4f57df94bcb76ef020905d086058a99ef5ad
Artur Cholewski <arti6603@gmail.com>
2024-11-26 17:44:35 +0100

    upload

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace HelloWorld
+namespace HelloWorld2
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Hello, World 2!");
+		
+			string nazwa_grupy = string.Empty;
+			while (nazwa_grupy == string.Empty)
+			{
+                Console.WriteLine("Podaj nazwe grupy:");
+                nazwa_grupy = Console.ReadLine();
+			}
+			
+			GrupaDydaktyczna gd = new GrupaDydaktyczna();
+			gd.NazwaGrupy = nazwa_grupy;
+			Console.WriteLine("Nazwa grupy: " + gd.NazwaGrupy);
 			
 			//Wykladowca wyk1 = new Wykladowca();
 			//wyk1.imie = "Tomasz";
@@ -32,7 +43,7 @@ namespace HelloWorld
 			stud2.nazwisko = "KUBIAK";
 			stud2.nr_albumu = 35669;
 			
-		  Student stud3 = new Student();
+		    Student stud3 = new Student();
 			stud3.imie = "Żaneta";
 			stud3.nazwisko = "LUBIŃSKA";
 			stud3.nr_albumu = 35900;
@@ -41,13 +52,18 @@ namespace HelloWorld
 			stud4.imie = "Krzysztof";
 			stud4.nazwisko = "WRZESIEŃ";
 			stud4.nr_albumu = 35678;
-			
 		
 			Console.WriteLine(stud1.nr_albumu + " " + stud1.imie + " " + stud1.nazwisko);
 			Console.WriteLine(stud2.nr_albumu + " " + stud2.imie + " " + stud2.nazwisko);
 			Console.WriteLine(stud3.nr_albumu + " " + stud3.imie + " " + stud3.nazwisko);
 			Console.WriteLine(stud4.nr_albumu + " " + stud4.imie + " " + stud4.nazwisko);
 			Console.WriteLine(wyk1.stopien_naukowy + " " + wyk1.imie + " " + wyk1.nazwisko);
+			
+			Console.WriteLine("Linia 62");
+			gd.Wykladowca = wyk1;
+			Console.WriteLine("Linia 64");
+			gd.DodajStudenta(stud1);
+			Console.WriteLine("Linia 66");
 		}
 	}
 	
@@ -69,11 +85,6 @@ namespace HelloWorld
 	  //string nazwisko;
 	  public string stopien_naukowy;
 	  
-	  public Wykladowca()
-	  {
-	    
-	  }
-	  
 	  public Wykladowca(string stopien_naukowy, string imie, string nazwisko)
 	  {
 	    this.imie = imie;
@@ -91,5 +102,23 @@ namespace HelloWorld
 	  {
 	    
 	  }
+	}
+	
+	class GrupaDydaktyczna
+	{
+	    private string nazwa_grupy;
+	    public string NazwaGrupy { get => nazwa_grupy; set => nazwa_grupy = value;}
+	    
+	    private Wykladowca wykladowca;
+	    public Wykladowca Wykladowca {get => wykladowca; set=>wykladowca=value;}
+	    
+	    private List<Student> studenci = new List<Student>(); 
+	    
+	    public void DodajStudenta(Student student)
+	    {
+	        Console.WriteLine("Linia 124");
+	        studenci.Add(student);
+	        Console.WriteLine("Linia 126");
+	    }
 	}
 }
